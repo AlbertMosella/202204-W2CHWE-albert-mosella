109a9e0040a75453f9793bd8a94dab22fd3560de
AlbertMosella <mosella.albert@gmail.com>
2022-04-16 01:20:23 +0200

    Added Grid generator function

---
+++ index.js
@@ -0,0 +1,17 @@
+const generateGrid = () => {
+  const gridRows = 20;
+  const gridColumns = 20;
+  const gridContainer = document.querySelector("#grid-container");
+  const grid = document.createElement("table");
+
+  for (let i = 0; i < gridRows; i++) {
+    const rows = document.createElement("tr");
+    for (let j = 0; j < gridColumns; j++) {
+      const columns = document.createElement("td");
+
+      rows.appendChild(columns);
+    }
+    grid.appendChild(rows);
+  }
+  gridContainer.appendChild(grid);
+};
