31b832ed44462eb362e2edf92f2829e51feb6276
AlbertMosella <mosella.albert@gmail.com>
2022-04-16 13:54:23 +0200

    Added matrix function

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const generateGrid = () => {
   const gridRows = 20;
-  const gridColumns = 20;
+  const gridcells = 20;
   const gridContainer = document.querySelector("#grid-container");
   const grid = document.createElement("table");
 
   for (let i = 0; i < gridRows; i++) {
     const rows = document.createElement("tr");
-    for (let j = 0; j < gridColumns; j++) {
-      const columns = document.createElement("td");
-
-      rows.appendChild(columns);
+    for (let j = 0; j < gridcells; j++) {
+      const cells = document.createElement("td");
+      cells.setAttribute("id", `${i}-${j}`);
+      cells.setAttribute("class", "dead");
+      rows.appendChild(cells);
     }
     grid.appendChild(rows);
   }
@@ -17,3 +18,14 @@ const generateGrid = () => {
 };
 
 generateGrid();
+
+const gridArray = [];
+const generateMatrix = () => {
+  for (let i = 0; i < 20; i++) {
+    gridArray[i] = [];
+    for (let j = 0; j < 20; j++) {
+      gridArray[i][j] = document.querySelector(`#${i}-${j}`);
+    }
+  }
+};
+generateMatrix();
