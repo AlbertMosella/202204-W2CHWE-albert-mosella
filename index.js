771693ba16f9783dfe0300803a87fa44d20c3191
AlbertMosella <mosella.albert@gmail.com>
2022-04-16 15:42:30 +0200

    Check cells function

--- index.js
+++ index.js
@@ -24,8 +24,40 @@ const generateMatrix = () => {
   for (let i = 0; i < 20; i++) {
     gridArray[i] = [];
     for (let j = 0; j < 20; j++) {
-      gridArray[i][j] = document.querySelector(`#${i}-${j}`);
+      gridArray[i][j] = document.getElementById(`${i}-${j}`);
     }
   }
+  return gridArray;
 };
 generateMatrix();
+
+const checkCellsAround = (x, y) => {
+  let cellsAroundCondition = 0;
+  if (gridArray[x - 1][y].className === "alive") {
+    cellsAroundCondition++;
+  }
+  if (gridArray[x + 1][y].className === "alive") {
+    cellsAroundCondition++;
+  }
+  if (gridArray[x - 1][y - 1].className === "alive") {
+    cellsAroundCondition++;
+  }
+  if (gridArray[x - 1][y + 1].className === "alive") {
+    cellsAroundCondition++;
+  }
+  if (gridArray[x][y - 1].className === "alive") {
+    cellsAroundCondition++;
+  }
+  if (gridArray[x][y + 1].className === "alive") {
+    cellsAroundCondition++;
+  }
+  if (gridArray[x + 1][y - 1].className === "alive") {
+    cellsAroundCondition++;
+  }
+  if (gridArray[x + 1][y + 1].className === "alive") {
+    cellsAroundCondition++;
+  }
+  return cellsAroundCondition;
+};
+
+checkCellsAround();
